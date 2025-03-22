914ea0b51aab3dc5202b0dd55ac94daaab4fbdb6
PBA443 <donbhagy@gmail.com>
2025-03-22 20:38:57 +0530

    add movies components

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = () => {};
+
+  render() {
+    return (
+      <>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button className="btn-danger btn-sm">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
--- vidly/src/main.jsx
+++ vidly/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
+import { Component, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import Movies from "./components/movies.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Movies></Movies>
   </StrictMode>
 );
