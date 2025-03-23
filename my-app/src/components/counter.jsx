84c3c6bf829dae888ea63882e03906c2f6922624
PBA443 <donbhagy@gmail.com>
2025-03-23 12:38:41 +0530

    child and parent single state of truth

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
   render() {
     return (
       <div>
-        <span className="badge badge-warning m-2">{this.formatCount()}</span>
-        <button onClick={() => this.handleIncrement({ id: 1 })}>
+        <span className="badge bg-warning text-dark">{this.formatCount()}</span>
+        <button
+          type="button"
+          className="btn btn-outline-success"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+        >
           Increment
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
         </button>
       </div>
     );
   }
   formatCount() {
-    return this.state.count === 0 ? "OhMyGod" : this.state.count;
+    const { value } = this.props.counter;
+    return value === 0 ? "OhMyGod" : value;
   }
 }
 
