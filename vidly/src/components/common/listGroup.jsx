3b8edc142f84a06299aba2e59556a6ca3aa36ece
PBA443 <donbhagy@gmail.com>
2025-03-24 16:54:01 +0530

    destructuring arguments

---
+++ vidly/src/components/common/listGroup.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ListGroup = ({
+  items,
+  textProperty = "name",
+  valueProperty = "_id",
+  onItemSelect,
+  selectedItem,
+}) => {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          style={{ cursor: "pointer" }}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListGroup;
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -45,8 +45,7 @@ class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+  getPageData = () => {
     const {
       currentPage,
       pageSize,
@@ -55,14 +54,23 @@ class Movies extends Component {
       sortColumn,
     } = this.state;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
-
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
         : allMovies;
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
     const movies = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { currentPage, pageSize, sortColumn } = this.state;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const { totalCount, data: movies } = this.getPageData();
 
     return (
       <>
@@ -75,7 +83,7 @@ class Movies extends Component {
             ></ListGroup>
           </div>
           <div className="col">
-            <p>Showing {filtered.length} movies in the database.</p>
+            <p>Showing {totalCount} movies in the database.</p>
             <MoviesTable
               movies={movies}
               sortColumn={sortColumn}
@@ -84,7 +92,7 @@ class Movies extends Component {
               onSort={this.handleSort}
             ></MoviesTable>
             <Pagination
-              itemsCount={filtered.length}
+              itemsCount={totalCount}
               pageSize={pageSize}
               currentPage={currentPage}
               onPageChange={this.handlePageChange}
