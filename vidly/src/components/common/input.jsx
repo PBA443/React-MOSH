9c89e4342bad5afbbc9130b5feb32a5d6263b813
PBA443 <donbhagy@gmail.com>
2025-03-25 17:15:17 +0530

    refactoring and error correction of rest

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -4,7 +4,7 @@ const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input {...rest} className="form-control" id={name}></input>
+      <input {...rest} name={name} className="form-control" id={name}></input>
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import Input from "./common/input";
+import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
