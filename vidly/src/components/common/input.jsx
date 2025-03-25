60b340076cfcfae7e8b615c6ee60e8b143c64766
PBA443 <donbhagy@gmail.com>
2025-03-25 16:58:11 +0530

    use rest operator for Input

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,26 +1,10 @@
 import React from "react";
 
-const Input = ({
-  name,
-  label,
-  value,
-  onChange,
-  placeholder,
-  type = "text",
-  error,
-}) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        name={name}
-        type={type}
-        className="form-control"
-        id={name}
-        placeholder={placeholder}
-      ></input>
+      <input {...rest} className="form-control" id={name}></input>
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
