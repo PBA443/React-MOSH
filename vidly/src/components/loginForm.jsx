581d487acb2a7e737822e73f9fb464ccce364529
PBA443 <donbhagy@gmail.com>
2025-03-25 16:58:31 +0530

    restructer the login form

---
+++ vidly/src/components/loginForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Passsword"),
+  };
+
+  doSubmit = () => {
+    console.log("Submited");
+  };
+
+  render() {
+    return (
+      <div className="form-group">
+        <h1>Login</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Passsword")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
