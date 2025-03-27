0bfed36ddc7d455604a3de062517b45cfdc2e50e
PBA443 <donbhagy@gmail.com>
2025-03-27 11:42:15 +0530

    validate password

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -7,8 +7,14 @@ import { register } from "../services/userService";
 const RegisterForm = () => {
   const schema = {
     email: Joi.string().required().email().label("Email"),
-    password: Joi.string().min(5).required().label("Password"),
-    name: Joi.string().required().label("Name"),
+    password: Joi.string()
+      .min(8)
+      .regex(/[A-Z]/, "upper-case")
+      .regex(/[a-z]/, "lower-case")
+      .regex(/[^\w]/, "special character")
+      .regex(/[0-9]/, "number")
+      .label("Password"),
+    name: Joi.string().min(3).required().label("Name"),
   };
 
   const doSubmit = async (data) => {
